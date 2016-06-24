e8d5046842c64569827349d47756298fd0136b9f
SuzukiTakumaEW <finalfantasy0712kuria@gmail.com>
2016-06-24 13:59:22 +0000

    <コミットメッセージ>

--- TysonApp/db/seeds.rb
+++ TysonApp/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# coding: utf-8
+
+Title.create(:name => '宇宙に行った日', :sales_date => '2011-06-28')
+Title.create(:name => '観察日記', :sales_date => '2011-11-14')
